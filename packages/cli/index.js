bb9fe9073c4710976fee1d4925172ca25de42366
rodzy <49137701+rodzy@users.noreply.github.com>
2020-09-05 15:54:46 -0600

    Initial phase @ packages/cli done ✅

--- packages/cli/index.js
+++ packages/cli/index.js
@@ -1,1 +1,39 @@
-console.log('Hello from CLI :)')
+require("colors");
+const inquirer = require("inquirer");
+
+const principalQuestions = () => {
+  const message =
+    "\nWelcome to Neutral ✨\nThe interactive command line interface to overbuild your React applications\n";
+  console.log(message);
+  const questions = [
+    {
+      type: "input",
+      name: "appName",
+      message: "Give a name to your aplication?",
+    },
+    {
+      type: "list",
+      name: "appType",
+      message: "Select your type of React app",
+      choices: [
+        "create-react-app (Default)",
+        "create-react-app (Using webpack⚡)",
+      ],
+    },
+    {
+      type: "list",
+      name: "appLanguage",
+      message: "Select the language of preference",
+      choices: ["JavaScript", "TypeScript"],
+    },
+    {
+      type: "list",
+      name: "appManager",
+      message: "Which package manager do you want to use",
+      choices: ["npm", "yarn"],
+    },
+  ];
+  return inquirer.prompt(questions);
+};
+
+principalQuestions();
