465b19959bedfbcf5ecd1bf9ac6e7d9188675875
rodzy <49137701+rodzy@users.noreply.github.com>
2020-09-05 17:24:38 -0600

    Finished imports for routing generations ✅

---
+++ packages/cli/configs/TS/settings/index.js
@@ -0,0 +1,3 @@
+const router = require("./routers");
+
+module.exports = [router];
---
+++ packages/cli/configs/TS/settings/routers/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: "reactRouter",
+    question: "Would you like to add routing? (React Router Dom - v.5.1.2)",
+    dependencies: ["react-router-dom@latest"],
+    devDependencies: ["@types/react-router-dom", "@types/history"],
+    packageEntries: [],
+    templates: [],
+};
