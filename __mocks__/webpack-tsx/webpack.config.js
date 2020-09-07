d5abfec8f79ec85114a2d74501278a84f746ba6f
rodzy <49137701+rodzy@users.noreply.github.com>
2020-09-07 11:17:07 -0600

    ✅ Provisional webpack config for tsx bundle

--- __mocks__/webpack-tsx/webpack.config.js
+++ __mocks__/webpack-tsx/webpack.config.js
@@ -1,2 +1,36 @@
-const path = require('path');
+const path = require("path");
 
+module.exports = {
+    target: "web",
+    // Switch the mode between development | production | none
+    mode: "development",
+    entry: {
+        // eslint-disable-next-line no-undef
+        app: path.join(__dirname, "src", "index.tsx"),
+    },
+    output: {
+        // eslint-disable-next-line no-undef
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"],
+    },
+    /* The dev server options are optional switch the port or the base
+        in case your port is unavailable */
+    devServer: {
+        contentBase: "./",
+        port: 4000,
+    },
+};
