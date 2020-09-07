973c1a1d20df042ef5d9c29cdce9ddeeeeb13758
rodzy <49137701+rodzy@users.noreply.github.com>
2020-09-07 10:45:36 -0600

    [Fix] eslint problem when reading CommonJS files with react TSX

--- __mocks__/webpack-tsx/src/App.tsx
+++ __mocks__/webpack-tsx/src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-export const App = () => {
+export const App = (): JSX.Element => {
     return (
         <div>
             <h1>Hello from tsx using webpack</h1>
