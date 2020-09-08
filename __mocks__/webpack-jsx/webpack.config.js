d3db39c6fd11423cb4d7b6e2f381294e4fcd6a3e
rodzy <49137701+rodzy@users.noreply.github.com>
2020-09-07 18:18:06 -0600

    Added basic configuration files

---
+++ __mocks__/webpack-jsx/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+
+module.exports = {
+    target: "web",
+    // Switch the mode between development | production | none
+    mode: "development",
+    entry: {
+        // eslint-disable-next-line no-undef
+        app: path.join(__dirname, "src", "index.tsx"),
+    },
+    output: {
+        // eslint-disable-next-line no-undef
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ["file-loader"],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: ["file-loader"],
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"],
+    },
+    /* The dev server options are optional switch the port or the base
+        in case your port is unavailable */
+    devServer: {
+        contentBase: "./",
+        port: 4000,
+    },
+};
